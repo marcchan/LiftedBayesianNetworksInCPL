4a07e174c7263c7ee0f5c4d05809fd7bf7934d85
marcchan <xingjian.chen@campus.lmu.de>
2021-11-22 16:44:45 +0100

    check the pgmpy functions and save the BN model file

--- src/lbn/main.py
+++ src/lbn/main.py
@@ -1,14 +1,25 @@
-from pgmpy.models import BayesianModel
+from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
-model = BayesianModel(
-    [('Drives', 'Air_is_good'), ('Air_is_good', 'Fined'), ('Drives', 'Fined')])
-cpd_d = TabularCPD(variable='Drives', variable_card=5, values=[
-                   [0.0625], [0.25], [0.375], [0.25], [0.0625]])
-cpd_a = TabularCPD(variable='Air_is_good', variable_card=2, values=[
-    [0.8 * 0.0625, 0.8 * 0.25, 0.8 * 0.375, 0.6 * 0.25, 0.6 * 0.0625],
-    [0.2 * 0.0625, 0.2 * 0.25, 0.2 * 0.375, 0.4 * 0.25, 0.4 * 0.0625]],
+from pgmpy.inference import VariableElimination
+DAF_model = BayesianNetwork([('Drives', 'Air_is_good'), ('Air_is_good', 'Fined'), ('Drives', 'Fined')])
+
+cpd_d = TabularCPD(
+    variable='Drives',
+    variable_card=5,
+    values=[
+        [0.0625],
+        [0.25],
+        [0.375],
+        [0.25],
+        [0.0625]],
+    state_names={'Drives': ['0','1', '2','3','4']})
+cpd_a = TabularCPD(variable='Air_is_good',
+                   variable_card=2,
+                   values=[[0.8, 0.8, 0.8,0.6,0.6],
+                           [0.2,0.2, 0.2, 0.4,0.4]],
                    evidence=['Drives'],
-                   evidence_card=[5])
+                   evidence_card=[5],
+                   state_names={'Air_is_good': ['True', 'False'],'Drives': ['0','1', '2','3','4']})
 # The representation of CPD in pgmpy is a bit different than the CPD shown in the above picture. In pgmpy the colums
 # are the evidences and rows are the states of the variable. So the grade CPD is represented like this:
 #
@@ -17,16 +28,26 @@ cpd_a = TabularCPD(variable='Air_is_good', variable_card=2, values=[
 #    +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
 #    |  A_i_g  | A_i_g_F | A_i_g_T | A_i_g_F | A_i_g_T | A_i_g_F | A_i_g_T | A_i_g_F | A_i_g_T | A_i_g_F | A_i_g_T |
 #    +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-#    | Fined_T |
+#    | Fined_T |  0.3        0.1      0.3         0.1      0.3        0.1      0.8        0.1     0.8         0.1
 #    +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-#    | Fined_F |
+#    | Fined_F |  0.7         0.9       0.7       0.9     0.7         0.9         0.2     0.9     0.2         0.9
 #    +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
 
 cpd_f = TabularCPD(variable='Fined', variable_card=2, values=[
-    [0.005, 0.00375,  0.02 , 0.015, 0.03,0.0225, 0.015,0.08, 0.00375, 0.02],
-    [0.045, 0.00875, 0.18 , 0.035, 0.27,0.0525, 0.135, 0.02,0.03375,0.005]],
+    [0.1, 0.3, 0.1, 0.3,0.1, 0.3,0.1,0.8,0.1,0.8],
+    [0.9, 0.7, 0.9, 0.7,0.9,0.7,0.9,0.2,0.9,0.2]],
                    evidence=['Drives','Air_is_good'],
-                   evidence_card=[5,2])
+                   evidence_card=[5,2],
+                   state_names={'Fined': ['True', 'False'],'Air_is_good': ['True', 'False'],'Drives': ['0','1', '2','3','4']})
 
-model.add_cpds(cpd_d, cpd_a, cpd_f)
-print(model.check_model())
+DAF_model.add_cpds(cpd_d, cpd_a, cpd_f)
+# print(DAF_model.check_model())
+# print(DAF_model.edges())
+# print(DAF_model.get_cpds('Fined'))
+
+# print(DAF_model.get_cpds('Fined').values)
+infer = VariableElimination(DAF_model)
+# print(infer.query(['Fined'], evidence={'Air_is_good': 'False', 'Drives': '0'}))
+print(infer.query(['Fined','Drives','Air_is_good']))
+print(DAF_model.get_independencies())
+# DAF_model.save('model_file')

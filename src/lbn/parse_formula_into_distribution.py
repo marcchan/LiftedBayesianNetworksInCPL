a488d6da8378b57a0d5b6f3141a76719fb71a30d
marcchan <xingjian.chen@campus.lmu.de>
2021-12-13 23:55:30 +0100

    fix bug in recu_data func, TODO parse func

--- src/lbn/parse_formula_into_distribution.py
+++ src/lbn/parse_formula_into_distribution.py
@@ -1,63 +1,125 @@
-from lbn.input.network import *
+# from lbn.input.network import *
 from scipy.special import comb
 import numpy
 import re
 
-def parse_conditional_probability(condition: str, value_dict: dict):
-    for variable, value in value_dict.items():
-        if variable in condition:
-            if type(value) == bool:
-                condition.replace(variable, value)
-            elif type(value) == 'int' or 'float':
-                if bool(re.match(r'||'+variable+'.*||')) == True:
-                    return
+
+# def parse_conditional_probability(condition: str, val, value_dict: dict,state_name):
+#     for variable, value in value_dict.items():
+#         if '!' in condition:
+#             condition.replace('!','not')
+#         if variable in condition:
+#             if type(value) == bool:
+#                 condition.replace(variable, value)
+#             elif type(value) == 'int' or 'float':
+#                 if bool(re.match(r'||'+variable+'.*||')) == True:
+#                         # [0,1,2,3,4]  5-1 = 4
+#                         value_ = value / (len(state_name[variable]) -1)
+#                         condition = re.sub(variable,value_,condition)
+#                         condition = re.sub('||','', condition)
+#     if eval(condition):
+#         return
 
 
+def check_probability_from_distribution(
+        card_list, distribution, p_names, state_name):
+    evi_value_dict = {p_names[i]: card_list[i] for i in range(len(card_list))}
+    prob = None
+    for cond, val in distribution.items():
+        condition = cond
+        if '!' in condition:
+            condition.replace('!', 'not')
+        for variable, value in evi_value_dict.items():
+            if variable in condition:
+                if isinstance(value, bool):
+                    condition.replace(variable, value)
+                elif isinstance(value, int):
+                    if bool(
+                        re.search(
+                            r'.*\|\|' +
+                            variable +
+                            '.*\\|\\|',
+                            condition)):
+                        # [0,1,2,3,4]  5-1 = 4
+                        value_ = str(value / (len(state_name[variable]) - 1))
+                        condition = re.sub('\\|\\|', '', condition)
+                        print(f'Drives or variable is {value}\n')
+                        print(value_)
+                        print(condition)
+                        print(variable)
+                        condition = re.sub(variable, value_, condition)
+                        # condition.replace(variable,value_)
+        print(condition)
+        if eval(condition):
+            prob = val
+            print(val)
+            return prob
+    if prob is None:
+        return -1
 
-def check_probability_from_distribution(distribution, card_list, p_names):
-    evi_value_dict = {[p_names[i]] : card_list[i] for i in range(len(card_list))}
-    for cond, value in distribution.item():
-        return
-        # parse_logic(cond)
 
-
-
-def recursive_fill_data(res_matrix,distribution, card_list,p_names):
+def recursive_data(
+        card_list,
+        res_matrix,
+        distribution,
+        p_names,
+        state_name, depth):
     for i in range(len(res_matrix)):
-        card_list.append(i)
         if type(res_matrix[i]).__name__ == 'ndarray':
-            res_matrix[i] =  recursive_fill_data(res_matrix[i],distribution,card_list,p_names)
+            # in different value example [0,0][0,1] -> [1,0] [1,1]
+            if len(card_list) != depth:
+                card_list[depth] = i
+                depth += 1
+            else:
+                card_list.append(i)
+                depth += 1
+            res_matrix[i], depth = recursive_data(
+                card_list, res_matrix[i], distribution, p_names, state_name, depth)
         else:
-            res_matrix[i] = check_probability_from_distribution(distribution,card_list,p_names)
-    return res_matrix
+            if len(card_list) != depth + 1:
+                card_list.append(i)
+            else:
+                card_list[depth] = i
+            # for test
+            # res_matrix[i] = 1
+            res_matrix[i] = check_probability_from_distribution(
+                card_list, distribution, p_names, state_name)
+    depth -= 1
 
-def fill_data_into_values(node: Node, row: int, column: int, evidence: set, distrubution: dict,state_name: dict):
+    return res_matrix, depth
+
+
+def fill_data_into_values(
+        node,
+        row: int,
+        column: int,
+        evidence: set,
+        distribution: dict,
+        state_name: dict):
     if len(evidence) == 0:
         res_arr = []
-        phi: float = distrubution['self']
+        phi: float = distribution['self']
         if node.get_type() == 'int':
             res_arr = list(setup_bino_dist(phi, node.get_domain()).values())
             print(res_arr)
+
+        # TODO friend(x,y)
         elif node.get_type() == 'bool':
             res_arr = [phi, 1 - phi]
         return numpy.array(res_arr).reshape(row, column)
     else:
         if node.get_type() == 'bool':
-            cards =[]
+            cards = []
             p_names = []
             for p_name in evidence:
                 p_names.append(p_name)
                 cards.append(len(state_name[p_name]))
             res_matrix = numpy.zeros(cards)
-            res_matrix = recursive_fill_data(cards, res_matrix,distrubution, p_names)
-
-
-
-
-
-
-
-
+            card_list = []
+            res_matrix = recursive_data(
+                card_list, res_matrix, distribution, p_names, state_name,depth=0)
+            print(res_matrix)
+    return
 
 # def check_only_with_frequence(node1: Node, nodes: list) -> bool:
 #     node_counter, freq_node_counter = 0, 0
@@ -86,8 +148,5 @@ def setup_bino_dist(phi: float, domain: int) -> dict:
     return:[0.0625,0.25,0.375,0.25,0.0625]
 
     '''
-    return {index:comb(domain, index) * pow(phi, index) * pow(1 - phi,
+    return {index: comb(domain, index) * pow(phi, index) * pow(1 - phi,
             domain - index) for index in range(domain + 1)}
-
-
-
